82ba66194fb220b2a7cb28ac16f89326f9831da3
slurge <josias.txus.serna@gmail.com>
2020-11-25 00:34:09 -0600

    Generalizado para todos los formularios web

--- duauth.js
+++ duauth.js
@@ -1,23 +1,27 @@
-const input = document.getElementById('input');
-let user = document.querySelector('.duauth-user');
+const user = document.querySelector('.duauth-user');
+const pass_input = document.querySelector('.duauth-pass');
+const login_form = document.querySelector('.duauth-form')
 
 let tiempoUltimoKey = null;
 let agent = navigator.userAgent;
 let arrayCadencia = [];
 console.log(agent)
 // Cancelamos el evento de paste para que la gente escriba su contraseña
-input.addEventListener('paste', (evento) => evento.preventDefault());
+pass_input.addEventListener('paste', (evento) => evento.preventDefault());
+
+//cancelamos tambien el envio de formulario por defecto, para realizar la captura
+login_form.addEventListener('submit', function(evento){ evento.preventDefault(); mandar(); });
 
 // Usamos el evento de entrada para revisar si la entrada quedó vacia
 // y si es el caso reinicializamos el array de cadencia y el tiempo de la ultima pulsación
-input.addEventListener('input', (evento) => {
-  if ( !input.value.trim() ) {
+pass_input.addEventListener('input', (evento) => {
+  if ( !pass_input.value.trim() ) {
     tiempoUltimoKey = null;
     arrayCadencia = [];
   }
 });
 
-input.addEventListener('keydown', (evento) => {
+pass_input.addEventListener('keydown', (evento) => {
   // Evitamos las teclas de borrado
   if ( evento.key == 'Backspace' || evento.key == 'Delete' ) { return; }
   
@@ -35,7 +39,7 @@ input.addEventListener('keydown', (evento) => {
   // Hacemos algo con la diferencia de tiempo
   const diferenciaDeTiempo = tiempoActual - tiempoUltimoKey;
   arrayCadencia.push(diferenciaDeTiempo);
-  //console.log('cadencia', input.value, arrayCadencia);
+  //console.log('cadencia', pass_input.value, arrayCadencia);
   
   // console.log(diferenciaDeTiempo); // para DEBUG
   tiempoUltimoKey = tiempoActual;
@@ -44,16 +48,22 @@ input.addEventListener('keydown', (evento) => {
 function mandar() {
   console.log(user.value)
   var xmlhttp = new XMLHttpRequest();
-  var data = {'cadencia': arrayCadencia,
-                'agent' : agent,
-                'token' : '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b',
-                 'user' : user.value};
+  var data = {
+    'cadencia': arrayCadencia,
+    'agent' : agent,
+    'token' : '50e3a98aa53216d4c6faf15a9b250061851caef33be7e5a7a7bef961ce2cefcf',
+     'user' : user.value
+    };
   xmlhttp.open('POST','http://duat.test/api/aberapi', true);
   xmlhttp.setRequestHeader("Content-type", "application/json");
   xmlhttp.send(JSON.stringify(data));
+
+  login_form.submit();
+  
+  //debug log, quitar pls
   xmlhttp.onload = function(){
     console.log(xmlhttp.response)
   }
   console.log(xmlhttp);
-  //console.log('cadencia', input.value, arrayCadencia); 
+  //console.log('cadencia', pass_input.value, arrayCadencia); 
 }
