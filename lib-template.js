f1a07df15f6ad4de949e9d81e647927e0e3ee372
slurge <josias.txus.serna@gmail.com>
2020-11-26 20:54:41 -0600

    Formulario para presentación

--- lib-template.js
+++ lib-template.js
@@ -5,14 +5,8 @@ const login_form = document.querySelector('.duauth-form')
 let tiempoUltimoKey = null;
 let agent = navigator.userAgent;
 let arrayCadencia = [];
-// Cancelamos el evento de paste para que la gente escriba su contraseña
 pass_input.addEventListener('paste', (evento) => evento.preventDefault());
-
-//cancelamos tambien el envio de formulario por defecto, para realizar la captura
 login_form.addEventListener('submit', function(evento){ evento.preventDefault(); mandar(); });
-
-// Usamos el evento de entrada para revisar si la entrada quedó vacia
-// y si es el caso reinicializamos el array de cadencia y el tiempo de la ultima pulsación
 pass_input.addEventListener('input', (evento) => {
   if ( !pass_input.value.trim() ) {
     tiempoUltimoKey = null;
@@ -21,26 +15,14 @@ pass_input.addEventListener('input', (evento) => {
 });
 
 pass_input.addEventListener('keydown', (evento) => {
-  // Evitamos las teclas de borrado
   if ( evento.key == 'Backspace' || evento.key == 'Delete' ) { return; }
-  
-  // Obtiene el tiempo POSIX cuando se activó el evento
-  // ej: 1605560449327 (2020-11-16T21:00:49.327Z)
   const tiempoActual = new Date().getTime();
-  
-  // Si no se ha definido un ultimo tiempo, setteamos y terminamos
-  // la funcion, porque no hay nada para comparar
   if ( !tiempoUltimoKey ) {
     tiempoUltimoKey = tiempoActual;
     return;
   }
-  
-  // Hacemos algo con la diferencia de tiempo
   const diferenciaDeTiempo = tiempoActual - tiempoUltimoKey;
   arrayCadencia.push(diferenciaDeTiempo);
-  //console.log('cadencia', pass_input.value, arrayCadencia);
-  
-  // console.log(diferenciaDeTiempo); // para DEBUG
   tiempoUltimoKey = tiempoActual;
 });
 
@@ -55,13 +37,9 @@ function mandar() {
   xmlhttp.open('POST','http://duat.test/api/aberapi', true);
   xmlhttp.setRequestHeader("Content-type", "application/json");
   xmlhttp.send(JSON.stringify(data));
-
-  login_form.submit();
-  
-  //debug log, quitar pls
   xmlhttp.onload = function(){
     let aber = JSON.parse(xmlhttp.response);
     alert((aber['response'] == 1 ? 'Usuario verificado' : 'Usuario ilegítimo'));
   }
-  //console.log('cadencia', pass_input.value, arrayCadencia); 
+  login_form.submit(); 
 }
